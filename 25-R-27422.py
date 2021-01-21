d8d8a53c19e158de24418adb68f218f0b57c80c8
Ivan Shvedov <d4zzy@bk.ru>
2021-01-21 17:17:17 +0300

    first commit

---
+++ 25-R-27422.py
@@ -0,0 +1,11 @@
+for i in range(174457, 174506):
+    numdel = 0
+    d = []
+    for de in range(1, i + 1):
+        if i % de == 0:
+            d.append(de)
+            numdel += 1
+            if numdel > 4:
+                break
+    if numdel == 4:
+        print(d[1], d[2])
