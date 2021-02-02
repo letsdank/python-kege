f89a713de4b0cb5cc5e5d883a32f393cead12bae
Daniel <ichi2209@icloud.com>
2021-02-02 13:44:12 +0300

    Начало РешуЕГЭ 25

--- re/25/universal.py
+++ re/25/universal.py
@@ -3,3 +3,13 @@
 
 
 def start():
+    int start = 0
+    int end = 0
+
+    print("Введите начало диапозона:")
+    start = int(input())
+    print("Введите конец диапозона:")
+    end = int(input())
+    
+
+    
