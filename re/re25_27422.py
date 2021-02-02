23c75ce66a9f67fae0f6d6e4083073d285ca5e1e
zxc <skipper2203@gmail.com>
2021-02-02 13:34:38 +0300

    re25

---
+++ re/re25_27422.py
@@ -0,0 +1,25 @@
+'''УСЛОВИЕ:
+    Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [174457; 174505], числа,
+ имеющие ровно два различных натуральных делителя, не считая единицы и самого числа. Для каждого найденного 
+ числа запишите эти два делителя в две соседних столбца на экране с новой строки в порядке возрастания 
+ произведения этих двух делителей. Делители в строке также должны следовать в порядке возрастания.
+
+Например, в диапазоне [5; 9] ровно два различных натуральных делителя имеют числа 6 и 8, поэтому для 
+этого диапазона вывод на экране должна содержать следующие значения:'''
+
+
+
+#   Copyright 2021 LetsDank
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
