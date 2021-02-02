8e3f5c243240da3bb7c75322275c5bfd6d52365b
Daniel <ichi2209@icloud.com>
2021-02-02 14:36:20 +0300

    Разработка 25 задания ЕГЭ

--- re/25/universal.py
+++ re/25/universal.py
@@ -1,15 +1,15 @@
 #   Copyright 2021 LetsDank (see LICENSE in root folder)
 
 
-
 def start():
-    int start = 0
-    int end = 0
-
     print("Введите начало диапозона:")
     start = int(input())
     print("Введите конец диапозона:")
     end = int(input())
-    
 
+    i = start
+    for start in range(end-start+1)
+        if 
+        i += 1
     
+start()
